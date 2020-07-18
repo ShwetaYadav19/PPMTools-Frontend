98d375f5cfc3d89f265e4ab5a051ef73129da3ce
Shweta Yadav <yshweta@amazon.com>
2020-07-18 11:59:24 +0530

    Adding UPDATE Proh=ject functionality in frontend

--- src/components/Project/ProjectItem.js
+++ src/components/Project/ProjectItem.js
@@ -1,14 +1,15 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class ProjectItem extends Component {
   render() {
-    const {project} = this.props;
+    const { project } = this.props;
     return (
       <div className="container">
         <div className="card card-body bg-light mb-3">
           <div className="row">
             <div className="col-2">
-    <span className="mx-auto">{project.projectIdentifier}</span>
+              <span className="mx-auto">{project.projectIdentifier}</span>
             </div>
             <div className="col-lg-6 col-md-4 col-8">
               <h3>{project.projectName}</h3>
@@ -21,11 +22,12 @@ class ProjectItem extends Component {
                     <i className="fa fa-flag-checkered pr-1">Project Board </i>
                   </li>
                 </a>
-                <a href="#">
+                <Link to={`/updateProject/${project.projectIdentifier}`}>
                   <li className="list-group-item update">
                     <i className="fa fa-edit pr-1">Update Project Info</i>
                   </li>
-                </a>
+                </Link>
+
                 <a href="">
                   <li className="list-group-item delete">
                     <i className="fa fa-minus-circle pr-1">Delete Project</i>
